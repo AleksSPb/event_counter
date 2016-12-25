37aeb7e29074e95ed6441d4a735f00ac13a5c92b
Alexander Parkhomenko <paraleksi@gmail.com>
2016-12-25 22:21:38 +0300

    Edit readme.md Change test, make some comments

--- src/main/java/aparkhomenko/eventcounter/EventCounterImpLowMemory.java
+++ src/main/java/aparkhomenko/eventcounter/EventCounterImpLowMemory.java
@@ -8,8 +8,8 @@ import java.util.concurrent.atomic.AtomicIntegerArray;
  * Created by aleks on 24.12.2016.
  * Данное решение не является точным подсчетом, т.к. расчет выполняется по секундно
  * Значение текущей секунды обнуляется и производится его инкремент при получении события
- * Т.е. получение статистики за минуту будет включать события за 59 предыдущих секунд + произошедшие до момента запроса статистики
- * Данный вариант выбран в связи с его устойчивостью к большим нагрузкам
+ * Т.е. получение статистики за минуту будет включать события за 59 предыдущих секунд + произошедшие до момента запроса статистики события текущей секунды
+ * Данный вариант выбран в связи с его устойчивостью к большим нагрузкам.
  */
 public class EventCounterImpLowMemory implements IEventCounter {
     private static final int SECONDS_IN_MINUTE = 60;
@@ -17,8 +17,6 @@ public class EventCounterImpLowMemory implements IEventCounter {
     private static final int SECONDS_IN_DAY = 24 * SECONDS_IN_HOUR;
     private final AtomicIntegerArray circularCounterArray = new AtomicIntegerArray(SECONDS_IN_DAY);
     private volatile int currentSecond;
-    private static volatile boolean canIncrement = true;
-
     private static volatile EventCounterImpLowMemory INSTANCE;
 
     private EventCounterImpLowMemory() {
@@ -27,29 +25,41 @@ public class EventCounterImpLowMemory implements IEventCounter {
         secondUpdater.start();
     }
 
+    /**
+     *
+     * @return Single instance of class
+     */
     public static IEventCounter getInstance() {
         if (INSTANCE == null) {
             synchronized (EventCounterImpLowMemory.class) {
                 if (INSTANCE == null) {
                     INSTANCE = new EventCounterImpLowMemory();
-
                 }
             }
         }
         return INSTANCE;
     }
 
+    /**
+     * Increment counter for the current second in array
+     */
     public void registerEvent() {
-
         circularCounterArray.incrementAndGet(currentSecond);
     }
 
+    /**
+     * Change current second and set it value to 0
+     */
     synchronized void incrementTime() {
         int newCurrent = (currentSecond + 1) % SECONDS_IN_DAY;
         circularCounterArray.set(newCurrent, 0);
         currentSecond = newCurrent;
     }
 
+    /**
+     * Calculate sum of registered events for current and last 59 seconds
+     * @return Return calculated sum
+     */
     public long getLastMinuteCounter() {
         long minuteCounter = 0;
         int currentSecond = this.currentSecond;
@@ -58,7 +68,10 @@ public class EventCounterImpLowMemory implements IEventCounter {
         }
         return minuteCounter;
     }
-
+    /**
+     * Calculate sum of registered events for current and last 3599 seconds
+     * @return Return calculated sum
+     */
     public long getLastHourCounter() {
         long hourCounter = 0;
         int currentSecond = this.currentSecond;
@@ -67,7 +80,10 @@ public class EventCounterImpLowMemory implements IEventCounter {
         }
         return hourCounter;
     }
-
+    /**
+     * Calculate sum of registered events for current and last 86399 seconds
+     * @return Return calculated sum
+     */
     public long getLast24HoursCounter() {
         int dayCounter = 0;
         for (int i = 0; i < SECONDS_IN_DAY; i++) {
@@ -75,26 +91,31 @@ public class EventCounterImpLowMemory implements IEventCounter {
         }
         return dayCounter;
     }
+
+    /**
+     * Internal support class for every second increment time in main class
+     */
+    class SecondUpdater extends TimerTask {
+
+        private final EventCounterImpLowMemory eventCounter;
+        private final Timer timer = new Timer(true);
+        private static final int DELAY = 1000;// 1 second
+
+        SecondUpdater(EventCounterImpLowMemory eventCounter) {
+            this.eventCounter = eventCounter;
+        }
+
+        void start() {
+            timer.schedule(this, 0, DELAY);
+        }
+
+        @Override
+        public void run() {
+            eventCounter.incrementTime();
+        }
+    }
 }
 
-class SecondUpdater extends TimerTask {
 
-    private final EventCounterImpLowMemory eventCounter;
-    private final Timer timer = new Timer(true);
-    private static final int DELAY = 1000;
-
-    SecondUpdater(EventCounterImpLowMemory eventCounter) {
-        this.eventCounter = eventCounter;
-    }
-
-    void start() {
-        timer.schedule(this, 0, DELAY);
-    }
-
-    @Override
-    public void run() {
-        eventCounter.incrementTime();
-    }
-}
 
 
--- src/main/java/aparkhomenko/eventcounter/LongCounterTest.java
+++ src/main/java/aparkhomenko/eventcounter/LongCounterTest.java
@@ -1,5 +1,8 @@
 package aparkhomenko.eventcounter;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
@@ -7,19 +10,24 @@ import java.util.concurrent.Executors;
 
 /**
  * Created by aleks on 24.12.2016.
+ * Класс содержит в методе main тест для класса EventCounterImpLowMemory
+ * Тест вынесен из тестовых классов из-за длительного выполнения (более 1 минуты)
+ * Цель данного теста, чтобы проверить общее значение произошедших событий и значение за последнюю минуту отличалось от общего
  */
 public class LongCounterTest {
     public static void main(String args[]) {
+        DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         ExecutorService executor = Executors.newFixedThreadPool(10);
         IEventCounter realTimeCounter = EventCounterImpLowMemory.getInstance();
         final Random random = new Random();
-        final int TOTAL_EVENTS = 120000;
+        final int TOTAL_EVENTS = 50000;
         CountDownLatch countDownLatch = new CountDownLatch(TOTAL_EVENTS);
+        System.out.println("Start test - " + sdf.format(new Date()));
         for (int i = 0; i < TOTAL_EVENTS; i++) {
             executor.execute(() -> {
                         realTimeCounter.registerEvent();
                         try {
-                            Thread.sleep(random.nextInt(100));
+                            Thread.sleep(random.nextInt(50));
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
@@ -32,6 +40,8 @@ public class LongCounterTest {
         } catch (Exception e) {
 
         }
+        System.out.println("Fired events - " + TOTAL_EVENTS);
+        System.out.println("End test - " + sdf.format(new Date()));
         System.out.println("Last minute events - " + realTimeCounter.getLastMinuteCounter());
         System.out.println("Last hour events - " + realTimeCounter.getLastHourCounter());
         System.out.println("Last day events - " + realTimeCounter.getLast24HoursCounter());
